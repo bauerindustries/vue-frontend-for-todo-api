135fb26c07cde2a4935db6e967996f11b746c45c
bauerindustries <104348398+bauerindustries@users.noreply.github.com>
2022-11-17 17:14:35 +0000

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 const TodosApp = {
   data() {
     return {
-      todo_API_URI: 'http://localhost:3000/todos/',
+      // todo_API_URI: 'http://localhost:3000/todos/',
+      todo_API_URI: 'https://todos-rest-api-demo.onrender.com/todos',
       isLoading: false,
       enteredTodoText: '',
       todos: [],
